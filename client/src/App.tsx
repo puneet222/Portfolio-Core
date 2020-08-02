9708a56d07198814a2fbad1aa02722546d99f5eb
Puneet Aggarwal <aggarwal.puneet222@gmail.com>
2020-08-02 14:29:55 +0530

    Added scss support and basic dark theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button } from 'antd';
 import './App.scss';
+import { LIGHT_THEME, DARK_THEME } from './app.constants';
 
-function App() {
+declare global {
+  interface Window {
+    less: any;
+  }
+}
+
+const App = () => {
+
+  const [theme, setTheme] = useState(LIGHT_THEME)
+
+  const toggleTheme = () => {
+    if (theme === LIGHT_THEME) {
+      document.body.classList.add('dark');
+      setTheme(DARK_THEME);
+    } else {
+      document.body.classList.remove('dark');
+      setTheme(LIGHT_THEME)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Testing 1 2 3</h3>
+        <Button type="primary" onClick={toggleTheme}>Button</Button>
       </header>
-    </div>
+    </div >
   );
 }
 
---
+++ client/src/app.constants.ts
@@ -0,0 +1,2 @@
+export const LIGHT_THEME: string = "LIGHT";
+export const DARK_THEME: string = "DARK";
